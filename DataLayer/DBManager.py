42180459228332681bb32f4d6fff7cdd7d6d75ed
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2021-11-28 03:11:26 +0000

    added some exception handling to DBManager

--- DataLayer/DBManager.py
+++ DataLayer/DBManager.py
@@ -1,4 +1,3 @@
-import re
 import mysql.connector
 from mysql.connector import cursor
 from mysql.connector.cursor import MySQLCursorPrepared
@@ -7,21 +6,33 @@ from DataLayer.config import Config
 
 class DBManager:
     def __init__(self):
-        self.__db = mysql.connector.connect(user=Config.DATABASE_CONFIG['user'],password=Config.DATABASE_CONFIG['password'],host=Config.DATABASE_CONFIG['host'],db=Config.DATABASE_CONFIG['dbName'])
+        try:
+            self.__db = mysql.connector.connect(user=Config.DATABASE_CONFIG['user'],password=Config.DATABASE_CONFIG['password'],host=Config.DATABASE_CONFIG['host'],db=Config.DATABASE_CONFIG['dbName'])
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            self.__db = False
+            
 
     def executeStatement(self, query, params):
-        cursor = self.__db.cursor(prepared=True)
+        try:
+            cursor = self.__db.cursor(prepared=True)
 
-        cursor.execute(query,params)
-        cursor.close()
-        self.__db.commit()
+            cursor.execute(query,params)
+            cursor.close()
+            self.__db.commit()
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
 
     def executeSelectStatement(self, query, params):
-        cursor = self.__db.cursor(prepared=True)
+        try:
+            cursor = self.__db.cursor(prepared=True)
 
-        cursor.execute(query,params)
-        result = cursor.fetchall()
-        cursor.close()
-        return result
+            cursor.execute(query,params)
+            result = cursor.fetchall()
+            cursor.close()
+            return result
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return None
 
     
